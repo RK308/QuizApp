725daeba6204f48f2d292a597b14312ffd625f01
Roopa <kroopa308@yahoo.com>
2018-06-06 23:04:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.quizapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startTest(View view){
+        Button button = (Button) findViewById(R.id.start_Test);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent quizPage = new Intent(MainActivity.this,QuizPage.class);
+                startActivity(quizPage);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/android/quizapp/QuizPage.java
@@ -0,0 +1,8 @@
+package com.example.android.quizapp;
+
+/**
+ * Created by krk29 on 6/5/2018.
+ */
+
+public class QuizPage {
+}
